5bb62680bc73836ab519691f86b8d186008507db
Krieger251677 <69798247+Krieger251677@users.noreply.github.com>
2020-10-19 16:30:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ders2
+{
+    class Program
+    {
+        static void yaz(object a, int b)
+        {
+            for (; b > 0; b--) 
+            
+            Console.WriteLine(a);
+            
+        }
+        static void Main()
+        {
+            yaz("dene", 3);
+            Console.ReadLine();
+        }
+    }
+}
